87600d80f28107c735d0c2616cc3422cbce13160
LightSirius <54878053+LightSirius@users.noreply.github.com>
2022-06-30 10:15:54 +0900

    comit

--- src/message/message.gateway.ts
+++ src/message/message.gateway.ts
@@ -11,11 +11,17 @@ import type {
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import type { Server, Socket } from 'socket.io';
+import { Inject, LoggerService } from '@nestjs/common';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 @WebSocketGateway(5001, { transports: ['websocket'], namespace: 'chat' })
 export class MessageGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER) private logger: LoggerService,
+  ) {}
+
   @WebSocketServer()
   server: Server;
 
@@ -25,27 +31,30 @@ export class MessageGateway
   }
 
   async getConnectedUserCount(): Promise<number> {
-    const count = await this.server.allSockets();
-    return count.size;
+    const allSockets = await this.server.allSockets();
+    return allSockets.size;
   }
 
   initLogging() {
     setInterval(async () => {
-      console.log('[LOG] User count : ' + (await this.getConnectedUserCount()));
-      // console.log(this.socketMap);
+      // console.log('[LOG] User count : ' + (await this.getConnectedUserCount()));
+      this.logger.debug('User count : ' + (await this.getConnectedUserCount()));
     }, 10000);
   }
 
   afterInit(server: Server): void {
-    console.log('after Init');
+    // console.log('after Init');
+    this.logger.debug('after Init');
     this.initLogging();
   }
 
   handleConnection(socket: Socket): void {
-    console.log('[LOG] Connected : ' + socket.id);
+    // console.log('[LOG] Connected : ' + socket.id);
+    this.logger.debug('Connected : ' + socket.id);
   }
 
   handleDisconnect(socket: Socket): void {
-    console.log('[LOG] Disconnected : ' + socket.id);
+    // console.log('[LOG] Disconnected : ' + socket.id);
+    this.logger.debug('Disconnected : ' + socket.id);
   }
 }
--- src/message/message.module.ts
+++ src/message/message.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { MessageGateway } from './message.gateway';
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import { utilities as nestWinstonModuleUtilities } from 'nest-winston/dist/winston.utilities';
 
 @Module({
-  imports: [],
+  imports: [
+    WinstonModule.forRoot({
+      transports: [
+        new winston.transports.Console({
+          level: process.env.NODE_ENV === 'production' ? 'info' : 'silly',
+          format: winston.format.combine(
+            winston.format.timestamp(),
+            nestWinstonModuleUtilities.format.nestLike(MessageModule.name, {
+              prettyPrint: true,
+            }),
+          ),
+        }),
+      ],
+    }),
+  ],
   controllers: [],
   providers: [MessageGateway],
 })
